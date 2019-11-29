a84aefa1814124e2f72671a0a4003e19bf86efb9
anastasiapan <43236029+anastasiapan@users.noreply.github.com>
2019-11-30 00:56:54 +0100

    Update genious_math.py

--- scripts/genious_math.py
+++ scripts/genious_math.py
@@ -4,11 +4,10 @@
 import math
 
 # This function gives the solutions extracted directly from MATLAB
-# inputs: x1, x2 -> x coordinates of the 2 qr codes - robot frame  
-#         y1, y2 -> y coordinates of the 2 qr codes - robot frame 
-#         (w1,z1) (w2,z2) -> coordinates of the 2 qr codes - unknown frame  
+# inputs: (x1,y1) (x2,y2) -> QR coords - unknown frame
+#         (w1,z1) (w2,z2) -> QR coords - world frame
 
-def world_frame_calc(x1, y1, x2, y2, w1, z1, w2, z2):
+def unknown_frame_calc(x1, y1, x2, y2, w1, z1, w2, z2):
 	l1 = math.sqrt(x1**2 + y1**2) # Distance from unknown frame origin - QR 1
 	l2 = math.sqrt(x2**2 + y2**2) # Distance from unknown frame origin - QR 2
 
@@ -48,137 +47,53 @@ def world_frame_calc(x1, y1, x2, y2, w1, z1, w2, z2):
 			- z1*z2**2 - z1**2*z2 + z1**3 + z2**3 - 2*w1*w2*z1 - 2*w1*w2*z2)/(2*(w1**2 - 2*w1*w2 + w2**2 
 			+ z1**2 - 2*z1*z2 + z2**2)) )
 
-	world_frame = [w01, z01, w02, z02]
-	return world_frame
+	unknown_cand = [w01, z01, w02, z02]
+	return unknown_cand
 #####################################################################################################################
 
-# This function calculates the orientation of the world frame
-# inputs: (x,y) -> Coordinates according to the world frame
-#         (w,z) -> Coordinates according to the robot frame
-#         (w0,z0) -> Coordinates of world's frame origin according to the robot frame
+# This function calculates the orientation of the unknown frame
+# inputs: (x1,y1) (x2,y2) -> QR coords - unknown frame
+#         (w1,z1) (w2,z2) -> QR coords - world frame
+#         (w0,z0) -> Coordinates of the unknown's frame origin according to the world frame
 
-def rot(x,y,w,z,w0,z0):
-	theta = math.atan((z-z0)/(w-w0)) - math.atan(y/x)	
-	return theta
+def rot(x1, y1, x2, y2, w1, z1, w2, z2, w0, z0):
+    # limit -pi pi
+    theta1 = math.asin(((z1-z0)*x1 - y1*(w1-w0))/((w1-w0)**2+(z1-z0)**2))
+    sign = 1 if (theta1>=0) else -1	
+    if abs(theta1) > math.pi:
+        theta1 = theta1 - 2*math.pi*sign
+
+    theta2 = math.asin(((z2-z0)*x2 - y2*(w2-w0))/((w2-w0)**2+(z2-z0)**2))
+    sign = 1 if (theta2>=0) else -1	
+    if abs(theta2) > math.pi:
+        theta2 = theta2 - 2*math.pi*sign
+
+    if theta1 == 0 and theta2 == 0:
+        x1pi = (w1-w0)*math.cos(math.pi)+(z1-z0)*math.sin(math.pi)
+        y1pi = -(w1-w0)*math.sin(math.pi)+(z1-z0)*math.cos(math.pi)
+        if x1pi == x1 and y1pi == y1:
+            theta1 = 180
+            theta2 = 180
+        else:
+            theta1 = 0
+            theta2 = 0
+
+    rot = [theta1, theta2]
+
+    return rot
 #####################################################################################################################
 
-# Test 1 #
-x1 = -10
-y1 = -2
-x2 = -3
-y2 = -10
-w1 = 5
-z1 = 6
-w2 = 12
-z2 = -2
+def unknown_frame(x1, y1, x2, y2, w1, z1, w2, z2):
+	unknown_cand = unknown_frame_calc(x1, y1, x2, y2, w1, z1, w2, z2)
+	rot1 = rot(x1, y1, x2, y2, w1, z1, w2, z2, unknown_cand[0], unknown_cand[1])
+	rot2 = rot(x1, y1, x2, y2, w1, z1, w2, z2, unknown_cand[2], unknown_cand[3])
 
-world_frame_candidates = world_frame_calc(x1, y1, x2, y2, w1, z1, w2, z2)
-print (world_frame_candidates)
-
-#QR1 - first candidate
-rot1 = rot(x1,y1,w1,z1,world_frame_candidates[0],world_frame_candidates[1])
-print(math.degrees(rot1))
-
-#QR2 - first candidate
-rot1 = rot(x2,y2,w2,z2,world_frame_candidates[0],world_frame_candidates[1])
-print(math.degrees(rot1))
-
-#QR1 - second candidate
-rot2 = rot(x1,y1,w1,z1,world_frame_candidates[2],world_frame_candidates[3])
-print(math.degrees(rot2))
-
-#QR2 - second candidate
-rot2 = rot(x2,y2,w2,z2,world_frame_candidates[2],world_frame_candidates[3])
-print(math.degrees(rot2))
-###########################################################################
-
-# Test 2 #
-x1 = -33
-y1 = 8
-x2 = 3
-y2 = 24
-w1 = -5
-z1 = -6
-w2 = 31
-z2 = 10
-
-world_frame_candidates = world_frame_calc(x1, y1, x2, y2, w1, z1, w2, z2)
-print (world_frame_candidates)
-
-#QR1 - first candidate
-rot1 = rot(x1,y1,w1,z1,world_frame_candidates[0],world_frame_candidates[1])
-print(math.degrees(rot1))
-
-#QR2 - first candidate
-rot1 = rot(x2,y2,w2,z2,world_frame_candidates[0],world_frame_candidates[1])
-print(math.degrees(rot1))
-
-#QR1 - second candidate
-rot2 = rot(x1,y1,w1,z1,world_frame_candidates[2],world_frame_candidates[3])
-print(math.degrees(rot2))
-
-#QR2 - second candidate
-rot2 = rot(x2,y2,w2,z2,world_frame_candidates[2],world_frame_candidates[3])
-print(math.degrees(rot2))
-###########################################################################
-
-# Test 3 #
-# Let's add -45 degrees rotation anti-clockwise #
-x1 = 12.7279
-y1 = 7.0711
-x2 = -12.0208
-y2 = 10.6066
-w1 = -4
-z1 = 18
-w2 = -19
-z2 = -2
-
-world_frame_candidates = world_frame_calc(x1, y1, x2, y2, w1, z1, w2, z2)
-print (world_frame_candidates)
-
-#QR1 - first candidate
-rot1 = rot(x1,y1,w1,z1,world_frame_candidates[0],world_frame_candidates[1])
-print(math.degrees(rot1))
-
-#QR2 - first candidate
-rot1 = rot(x2,y2,w2,z2,world_frame_candidates[0],world_frame_candidates[1])
-print(math.degrees(rot1))
-
-#QR1 - second candidate
-rot2 = rot(x1,y1,w1,z1,world_frame_candidates[2],world_frame_candidates[3])
-print(math.degrees(rot2))
-
-#QR2 - second candidate
-rot2 = rot(x2,y2,w2,z2,world_frame_candidates[2],world_frame_candidates[3])
-print(math.degrees(rot2))
-###########################################################################
-
-# Test 4 #
-# +45 degrees rotation #
-y1 = 12.7279
-x1 = 7.0711
-y2 = -12.0208
-x2 = 10.6066
-w1 = -4
-z1 = 18
-w2 = -19
-z2 = -2
-
-world_frame_candidates = world_frame_calc(x1, y1, x2, y2, w1, z1, w2, z2)
-print (world_frame_candidates)
-
-#QR1 - first candidate
-rot1 = rot(x1,y1,w1,z1,world_frame_candidates[0],world_frame_candidates[1])
-print(math.degrees(rot1))
-
-#QR2 - first candidate
-rot1 = rot(x2,y2,w2,z2,world_frame_candidates[0],world_frame_candidates[1])
-print(math.degrees(rot1))
-
-#QR1 - second candidate
-rot2 = rot(x1,y1,w1,z1,world_frame_candidates[2],world_frame_candidates[3])
-print(math.degrees(rot2))
-
-#QR2 - second candidate
-rot2 = rot(x2,y2,w2,z2,world_frame_candidates[2],world_frame_candidates[3])
-print(math.degrees(rot2))
+	if round(rot1[0],4) == round(rot1[1],4):
+		unknown_frame = {"w0" : unknown_cand[0], "z0" : unknown_cand[1], "theta" : rot1[0]}
+		print(unknown_frame)
+	elif round(rot1[2],4) == round(rot1[3],4):
+		unknown_frame = {"w0" : unknown_cand[2], "z0" : unknown_cand[3], "theta" : rot2[0]}
+		print(unknown_frame)
+	else:
+		print ("Whoops! You fucked up somewhere!")
+#####################################################################################################################
