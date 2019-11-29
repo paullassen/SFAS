ebdaf000b62323f0f1bc72e7bd5d78f660a6b9bd
anastasiapan <43236029+anastasiapan@users.noreply.github.com>
2019-11-30 00:57:55 +0100

    Add files via upload

---
+++ scripts/test_math.py
@@ -0,0 +1,48 @@
+import math
+import genious_math
+
+# function test
+
+x1 = -9.5*math.sqrt(2)
+y1 = -4.5*math.sqrt(2)
+w1 = 4;
+z1 = 9;
+x2 = 4*math.sqrt(2)
+y2 = 2*math.sqrt(2)
+w2 = 24
+z2 = 2
+
+unknown_frame(x1, y1, x2, y2, w1, z1, w2, z2)
+
+x1 = 5.5*math.sqrt(2)
+y1 = -7.5*math.sqrt(2)
+w1 = 5
+z1 = 7
+x2 = -5*math.sqrt(2)
+y2 = -math.sqrt(2)
+w2 = -12
+z2 = 3
+
+unknown_frame(x1, y1, x2, y2, w1, z1, w2, z2)
+
+x1 = -12
+y1 = -5
+w1 = -3
+z1 = 13
+x2 = -6
+y2 = 10
+w2 = -9
+z2 = -2
+
+unknown_frame(x1, y1, x2, y2, w1, z1, w2, z2)
+
+x1 = -3;
+y1 = -4;
+w1 = 10;
+z1 = 7;
+x2 = 8;
+y2 = -7;
+w2 = 7;
+z2 = -4;
+
+unknown_frame(x1, y1, x2, y2, w1, z1, w2, z2)
