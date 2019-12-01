9d5638342dc332fc748f5818a7fc040a0de0187c
Paul Lassen <paullassen+github@protonmail.com>
2019-12-01 21:30:07 +0100

    updaates

--- scripts/genious_math.py
+++ scripts/genious_math.py
@@ -91,13 +91,22 @@ def unknown_frame(x1, y1, x2, y2, w1, z1, w2, z2):
 	#print(rot1)
 	rot2 = rot(x1, y1, x2, y2, w1, z1, w2, z2, unknown_cand[2], unknown_cand[3])
 	#print(rot2)
-
-	if round(rot1[0],4) == round(rot1[1],4):
-		unknown_frame = [unknown_cand[0], unknown_cand[1], rot1[0]]
-		#print(unknown_frame)
-	elif round(rot2[0],4) == round(rot2[1],4):
-		unknown_frame = [ unknown_cand[2],  unknown_cand[3], rot2[0]]
-		#print(unknown_frame)
+	unknown_frame = []
+	if unknown_cand[0] > 4.5 or unknown_cand[0] < -0.5 or abs(unknown_cand[1] > 4):
+		unknown_frame = [[ unknown_cand[2],  unknown_cand[3], rot2[0]],[unknown_cand[0], unknown_cand[1], rot1[0]]]
+	elif unknown_cand[2] > 4.5 or unknown_cand[2] < -0.5 or abs(unknown_cand[3] > 4):
+		unknown_frame = [[unknown_cand[0], unknown_cand[1], rot1[0]],[ unknown_cand[2],  unknown_cand[3], rot2[0]]]
 	else:
-		print ("Whoops! You fucked up somewhere!")
+		dif1 = abs(rot1[0]-rot1[1])
+		dif2 = abs(rot2[0]-rot2[0])
+
+		if dif1<=dif2:
+			unknown_frame = [[unknown_cand[0], unknown_cand[1], rot1[0]],[ unknown_cand[2],  unknown_cand[3], rot2[0]]]
+			print(unknown_frame)
+		elif dif2<dif1:
+			unknown_frame = [[ unknown_cand[2],  unknown_cand[3], rot2[0]],[unknown_cand[0], unknown_cand[1], rot1[0]]]
+			print(unknown_frame)
+		else:
+			print ("Whoops! You fucked up somewhere!")
+	return unknown_frame
 #####################################################################################################################
--- scripts/qr_coords.py
+++ scripts/qr_coords.py
@@ -7,32 +7,34 @@ import std_msgs.msg as stm
 import tf2_msgs.msg as tfm
 import tf.transformations as tft
 
-from genius_math import world_frame_calc
+from genious_math import unknown_frame
 
 class qr_store:
-	def __init__(known_loc, unknown_loc, unknown_next, num, letter):
+	def __init__(self,known_loc, unknown_loc, unknown_next, num, letter):
 		self.world_loc = known_loc
 		self.frame_loc = unknown_loc
 		self.next_qr = unknown_next
 		self.num = num
 		self.letter = letter
-
+		self.robot = []
 class Wasp:
 	def __init__(self):
 		self.coords= [0.0,0.0]
 		self.status=0	
-		self.odom = [0.0, 0.0]
+		self.odom = [0.0,0.0]
 		self.rotation = 0
 		self.qr_coords = [0.0, 0.0]
 		self.msg = []
 		self.qr_store = [0, 0, 0, 0, 0]
 		self.flag  = 0
 		self.nums = []
-		self.frame_origin
-		self.world_frame_candidates = []
+		self.frame_origin = []
+		self.max_cov = 1
 
 	def update_pos(self,msg):
-		pos = msg.pose.position
+		pos = msg.pose.pose.position
+		cov = msg.pose.covariance
+		self.max_cov = max(cov)
 		self.coords[0] = math.sqrt(0.0**2 + pos.z**2)
 		self.coords[1] = -pos.x + 0.0025
 		self.qr_coords[0] = self.odom[0] + self.coords[0]*math.cos(self.rotation) - self.coords[1]*math.sin(self.rotation)
@@ -65,51 +67,69 @@ class Wasp:
 			self.add_qr(msg)
 
 	def add_qr(self, msg):
-		if msg.data not in self.msg and self.flag > 0 and msg.data != '':
+		if msg.data not in self.msg and self.flag > 0 and msg.data != '' and self.max_cov < 3e-04:
 			self.msg.append(msg.data)
-			self.handle_message(msg.data, self.qr_coords[0], self.qr_coords[1])
+			self.handle_message(msg.data, (self.qr_coords[0], self.qr_coords[1]))
 			self.flag = 0
 			if len(self.nums) == 2:
 				tmp = []
 				for num in self.nums:
 					tmp.append(self.qr_store[num].world_loc)
 					tmp.append(self.qr_store[num].frame_loc)
-				avg_x = (tmp[0][0]-tmp[1][0] + tmp[0][1]-tmp[1][1])/2
-				avg_y = (tmp[2][0]-tmp[3][0] + tmp[2][1]-tmp[3][1])/2
-				self.frame_origin = (avg_x, avg_y)
-
+				self.frame_origin = unknown_frame(tmp[1][0], tmp[1][1], tmp[3][0], tmp[3][1], tmp[0][0], tmp[0][1], tmp[2][0], tmp[2][1])
+				self.frame_origin[0][1] = -self.frame_origin[0][1]
 	def print_coords(self):
-		print self.qrs
-"""
-		print '\r========================================'
-		print '\rCurrent Location:' 
-		print '\r========================================'
-		print '\r\tx: {}\n\ty: {}'.format(self.odom[0], self.odom[1])
-		print '\r========================================'
-		if self.status > 2:
-			print '\rQR Location:      Status: Visible'
-		else:
-			print '\rQR Location:      Status: Invisible'	
-		print '\r========================================'
-		print '\r\tx: {}\n\ty: {}'.format(self.qr_coords[0], self.qr_coords[1])
-		#print '\r\033[10A'
-"""
+		for num in self.nums:
+			print self.qr_store[num].robot
+			print self.qr_store[num].world_loc
+			print self.qr_store[num].frame_loc
+			print "==========="
+		if len(self.nums) >= 2:
+			print self.frame_origin
+			print "----------"
+		print self.max_cov 
+		
 	def handle_message(self, data, known_loc):
 		inf=data.split("\r\n")
 		unknown_loc = (float(inf[0][2:]),  float(inf[1][2:]))
 		unknown_next = (float(inf[2][7:]), float(inf[3][7:]))
-		num = int(inf[4][2:])
-		letter = msgs[5][2:]
+		num = int(inf[4][2:])-1
+		letter = inf[5][2:]
 		self.nums.append(num)
 		self.qr_store[num] = qr_store(known_loc, unknown_loc, unknown_next, num, letter)
+		self.qr_store[num].robot = (self.coords[0], self.coords[1])
+	def next_loc(self):
+		ind = min(self.nums)
+
+		while ind in self.nums:
+			ind = (ind + 1) % 5
+
+		nxt = self.qr_store[ind-1].next_qr
+		nxt_x = self.frame_origin[0][0]+nxt[0]*math.cos(self.frame_origin[0][2])-math.sin(self.frame_origin[0][2])*nxt[1]
+		nxt_y = self.frame_origin[0][1]+nxt[0]*math.sin(self.frame_origin[0][2])+math.cos(self.frame_origin[0][2])*nxt[1]
+		nxt = (nxt_x, nxt_y, ind+1)
+		return nxt	
+
+	def pub_loc(self,publisher):
+		pub = geo.Pose()
+		if len(self.nums)<2:
+			pub.position.x = -10
+			pub.position.y = -10
+			pub.position.z = -10
+		else:
+			nxt = self.next_loc()
+			pub.position.x = nxt[0]
+			pub.position.y = nxt[1]
+			pub.orientation.x = nxt[2]
+		publisher.publish(pub)
 def visp_callback(msg, callback_arg):
 	some_code = msg
 	wsp = callback_arg[0]
 	var = callback_arg[1]
 	wsp.update(msg, var)
 if __name__ == '__main__':
+	pub = rospy.Publisher('nxt_loc', geo.Pose, queue_size=1)
 	rospy.init_node('wasp')
-	rospy		
 	wsp = Wasp()	
 	
 	pos_var = 1
@@ -117,8 +137,12 @@ if __name__ == '__main__':
 	tf_var = 3
 	message_var = 4
 	
-	rospy.Subscriber("visp_auto_tracker/object_position",geo.PoseStamped,visp_callback, (wsp, pos_var))
+	rate = rospy.Rate(3)	
+	rospy.Subscriber("visp_auto_tracker/object_position_covariance",geo.PoseWithCovarianceStamped,visp_callback, (wsp, pos_var))
 	rospy.Subscriber("visp_auto_tracker/status",stm.Int8,visp_callback, (wsp, status_var))
 	rospy.Subscriber("visp_auto_tracker/code_message",stm.String,visp_callback, (wsp, message_var))
 	rospy.Subscriber("amcl_pose",geo.PoseWithCovarianceStamped, visp_callback, (wsp, tf_var))
-	rospy.spin()
+
+	while not rospy.is_shutdown():
+		wsp.pub_loc(pub)
+		rate.sleep()
